af3e026fb12262aa81e74f171580e39f5e1387a1
Talen-520 <63370853+Talen-520@users.noreply.github.com>
2021-11-08 16:32:10 -0500

    Create ListIterator.java

---
+++ ListIterator.java
@@ -0,0 +1,14 @@
+public interface ListIterator<AnyType>
+{
+  boolean hasPrevious();
+
+  boolean hasNext();
+
+  AnyType previous();
+
+  AnyType next();
+
+  void add(AnyType newValue);
+
+  void remove();
+}
